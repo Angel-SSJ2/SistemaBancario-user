6542f35a46ac29a2890c2f1903b088423f73d8ec
AADLCC <adelacruz-2021326@kinal.edu.gt>
2026-02-15 16:13:09 -0600

    Modulos de usuarios y transacciones agregadas

--- src/app.js
+++ src/app.js
@@ -2,18 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const { dbConnection } = require('./configs/db');
+const userRoutes = require('./routes/user.routes.js');
+const transactionRoutes = require('./routes/transactions.routes.js');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
-app.use('/api/transactions', require('./routes/transaction.routes'));
+app.use('/api/transactions', transactionRoutes);
+app.use('/api/users', userRoutes);
 
 // Conectar a base de datos
 dbConnection();
 
-// Middlewares
-app.use(express.json());
-app.use(cors());
-app.use(morgan('dev'));
 
 
 module.exports = app;
